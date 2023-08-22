84a92f45c6a7f97dbd1422ee12ece5f0b26e0765
willl85 <ngowilfried@gmail.com>
2023-08-22 15:15:12 +0200

    Remove log

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -5,7 +5,7 @@ import classes from './Navigation.module.css';
 
 const Navigation = () => {
   const ctx = useContext(AuthContext);
-  console.log(ctx);
+  
   return (
     <nav className={classes.nav}>
       <ul>
