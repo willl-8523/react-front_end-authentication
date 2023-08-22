34f0161ac86a3e693e2a280350eefaa9d773ecfb
willl85 <ngowilfried@gmail.com>
2023-08-22 15:13:35 +0200

    Remove Button component and AuthContext

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
-import AuthContext from '../context/auth-context';
-import Button from '../UI/Button/Button';
 
 const Home = () => {
-  const authCtx = useContext(AuthContext);
 
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={authCtx.onLogout}></Button>
     </Card>
   );
 };
