a7d5065c733829f40705c912e29f5d0c92dce20e
willl85 <ngowilfried@gmail.com>
2023-08-22 14:21:52 +0200

    First way to use useContext

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-
-import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/context/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,13 +28,18 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        // state isLoggedIn to change the value to property isLoggedIn on AuthContext
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
